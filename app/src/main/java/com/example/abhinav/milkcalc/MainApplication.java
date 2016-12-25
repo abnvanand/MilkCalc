130a1eea98fd8e50acccaf92dc6a1be5077a264a
Abhinav Anand <abhinav.anand@veris.in>
2016-12-25 19:47:01 +0530

    chore: plant DebugTree for Timber

--- app/src/main/java/com/example/abhinav/milkcalc/MainApplication.java
+++ app/src/main/java/com/example/abhinav/milkcalc/MainApplication.java
@@ -2,10 +2,13 @@ package com.example.abhinav.milkcalc;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree());
     }
 }
