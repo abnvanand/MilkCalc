264d4939bb4c437ccbec284b3456aaced1941df4
Abhinav Anand <abhinav.anand@veris.in>
2016-12-25 19:32:59 +0530

    chore: add data binding support

--- app/src/main/java/com/example/abhinav/milkcalc/MainActivity.java
+++ app/src/main/java/com/example/abhinav/milkcalc/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.abhinav.milkcalc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.abhinav.milkcalc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
